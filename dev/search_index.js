33f939ccd00c24c26472fd0b6832c7a7b5ff0d65
Documenter.jl <documenter@juliadocs.github.io>
2020-10-06 03:10:38 +0000

    build based on cf575f2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"dummytest/#Dummy","page":"Dummy","title":"Dummy","text":"","category":"section"},{"location":"dummytest/","page":"Dummy","title":"Dummy","text":"dummy","category":"page"},{"location":"#This-is-a-test-document","page":"Home","title":"This is a test document","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Test","category":"page"}]
+}
