0b811441e1f20801a3fb5a0fc8170447c6a080cf
Spark Tseung <hi@sparktseung.com>
2020-11-15 03:41:44 -0500

    DataFrame->Array in simulation

--- src/simulation.jl
+++ src/simulation.jl
@@ -3,11 +3,13 @@ function sim_components(model, sample_size)
 end
 
 function sim_logit_gating(α, X)
+    X = Array(X)
     probs = exp.(LogitGating(α, X))
     return hcat([rand(Distributions.Multinomial(1, probs[i,:])) for i in 1:size(X)[1]]...)'
 end
 
 function sim_dataset(α, X, model)
+    X = Array(X)
     dim_comp_sim = sim_components(model, size(X)[1])
     gating_sim = sim_logit_gating(α, X)
     return hcat([sum(gating_sim .* dim_comp_sim[j], dims = 2)  for j in 1:size(model)[1]]...)
