c17967242c860d2cec61ff7f933f094ed0bfe91c
Spark Tseung <hi@sparktseung.com>
2021-05-19 07:08:32 -0400

    Finished: ZINB, exact

--- test/fit_functions_new.jl
+++ test/fit_functions_new.jl
@@ -93,43 +93,87 @@ end
 
 @testset "NegativeBinomial, exact" begin
 
-    # Fit 20,000 observations as an example
+    # Fit 50,000 observations as an example
 
-    param0 = rand(Distributions.Uniform(0, 1), 1)[1]
-    param1 = rand(Distributions.Uniform(0.5, 8), 4)
-    param2 = rand(Distributions.Uniform(0, 1), 4)
+    # param0 = rand(Distributions.Uniform(0, 1), 1)[1]
+    # param1 = rand(Distributions.Uniform(0.5, 8), 4)
+    # param2 = rand(Distributions.Uniform(0, 1), 4)
 
-    X = rand(Distributions.Uniform(-5, 5), 50000, 7)
-    α = rand(Distributions.Uniform(-1, 1), 2, 7)
-    α[2,:] .= 0
+    # X = rand(Distributions.Uniform(-5, 5), 50000, 7)
+    # α = rand(Distributions.Uniform(-1, 1), 2, 7)
+    # α[2,:] .= 0
 
-    model = [NegativeBinomialExpert(param1[1], param2[1]) NegativeBinomialExpert(param1[2], param2[2]);
-             NegativeBinomialExpert(param1[3], param2[3]) NegativeBinomialExpert(param1[4], param2[4])]
+    # model = [NegativeBinomialExpert(param1[1], param2[1]) NegativeBinomialExpert(param1[2], param2[2]);
+    #          NegativeBinomialExpert(param1[3], param2[3]) NegativeBinomialExpert(param1[4], param2[4])]
 
-    expos = rand(Distributions.Uniform(0.1, 5), 50000)
-    Y_sim = LRMoE.sim_dataset(α, X, model, exposure = expos)
+    # expos = rand(Distributions.Uniform(0.1, 5), 50000)
+    # Y_sim = LRMoE.sim_dataset(α, X, model, exposure = expos)
 
-    α_guess = fill(0.0, 2, 7)
-    model_guess = [NegativeBinomialExpert(0.5*param1[1], 0.5) NegativeBinomialExpert(3*param1[2], 0.5);
-                   NegativeBinomialExpert(2*param1[3], 0.5) NegativeBinomialExpert(0.2*param1[4], 0.5)]
+    # α_guess = fill(0.0, 2, 7)
+    # model_guess = [NegativeBinomialExpert(0.5*param1[1], 0.5) NegativeBinomialExpert(3*param1[2], 0.5);
+    #                NegativeBinomialExpert(2*param1[3], 0.5) NegativeBinomialExpert(0.2*param1[4], 0.5)]
 
-    pen_α = 5
-    pen_params = [[[1.0 Inf], [1.0 Inf]],
-                  [[1.0 Inf], [1.0 Inf]]]
+    # pen_α = 5
+    # pen_params = [[[1.0 Inf], [1.0 Inf]],
+    #               [[1.0 Inf], [1.0 Inf]]]
 
-    result = LRMoE.fit_exact(Y_sim, X, α_guess, model_guess,
-                        exposure = expos,
-                        penalty = true, 
-                        pen_α = pen_α, pen_params = pen_params,
-                        # ϵ = 1e-03, α_iter_max = 5.0, ecm_iter_max = 200,
-                        # grad_jump = true, grad_seq = nothing,
-                        print_steps = true)
+    # result = LRMoE.fit_exact(Y_sim, X, α_guess, model_guess,
+    #                     exposure = expos,
+    #                     penalty = true, 
+    #                     pen_α = pen_α, pen_params = pen_params,
+    #                     # ϵ = 1e-03, α_iter_max = 5.0, ecm_iter_max = 200,
+    #                     # grad_jump = true, grad_seq = nothing,
+    #                     print_steps = true)
     
-    α
-    result.α_fit 
+    # α
+    # result.α_fit 
    
-    model
-    result.model_fit
+    # model
+    # result.model_fit
+
+
+
+end
+
+@testset "ZINegativeBinomial, exact" begin
+
+    # Fit 50,000 observations as an example
+
+    # param0 = [0.25 0.30 0.80 0.75] # rand(Distributions.Uniform(0, 1), 4)
+    # param1 = [3 2.5 8 10] # rand(Distributions.Uniform(0.5, 8), 4)
+    # param2 = [0.75 0.38 0.26 0.5]# rand(Distributions.Uniform(0, 1), 4)
+
+    # X = rand(Distributions.Uniform(-5, 5), 50000, 7)
+    # α = rand(Distributions.Uniform(-1, 1), 2, 7)
+    # α[2,:] .= 0
+
+    # model = [ZINegativeBinomialExpert(param0[1], param1[1], param2[1]) ZINegativeBinomialExpert(param0[2], param1[2], param2[2]);
+    #          ZINegativeBinomialExpert(param0[3], param1[3], param2[3]) ZINegativeBinomialExpert(param0[4], param1[4], param2[4])]
+
+    # expos = rand(Distributions.Uniform(0.1, 5), 50000)
+    # Y_sim = LRMoE.sim_dataset(α, X, model, exposure = expos)
+
+    # α_guess = fill(0.0, 2, 7)
+    # model_guess = [ZINegativeBinomialExpert(0.5, 0.5*param1[1], 0.5) ZINegativeBinomialExpert(0.5, 3*param1[2], 0.5);
+    #                ZINegativeBinomialExpert(0.5, 2*param1[3], 0.5) ZINegativeBinomialExpert(0.5, 0.2*param1[4], 0.5)]
+
+    # pen_α = 5
+    # pen_params = [[[1.0 Inf], [1.0 Inf]],
+    #               [[1.0 Inf], [1.0 Inf]]]
+
+    # result = LRMoE.fit_exact(Y_sim, X, α_guess, model_guess,
+    #                     exposure = expos,
+    #                     penalty = true, 
+    #                     pen_α = pen_α, pen_params = pen_params,
+    #                     # ϵ = 1e-03, α_iter_max = 5.0, ecm_iter_max = 200,
+    #                     # grad_jump = true, grad_seq = nothing,
+    #                     print_steps = true)
+    
+    # α
+    # result.α_fit 
+   
+    # model
+    # result.model_fit
 
 
 
