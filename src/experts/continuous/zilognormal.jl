66e130a95635d03fbb35bcb2f63c8aca55923f36
sparktseung <hi@sparktseung.com>
2022-09-04 16:16:18 -0400

    fix penalty term

--- src/experts/continuous/zilognormal.jl
+++ src/experts/continuous/zilognormal.jl
@@ -114,7 +114,7 @@ end
 ## penalty
 penalty_init(d::ZILogNormalExpert) = [Inf 2.0 Inf]
 no_penalty_init(d::ZILogNormalExpert) = [Inf 1.0 Inf]
-penalize(d::ZILogNormalExpert, p) = 0 # (d.μ/p[1])^2 + (p[2]-1)*log(d.σ) - d.σ/p[3]
+penalize(d::ZILogNormalExpert, p) = (p[2]-1)*log(d.σ) # (d.μ/p[1])^2 + (p[2]-1)*log(d.σ) - d.σ/p[3]
 
 ## statistics
 mean(d::ZILogNormalExpert) = (1 - d.p) * mean(Distributions.LogNormal(d.μ, d.σ))
