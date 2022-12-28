4ee5c6ed3ddbdd7f117a14404aa4664621439f4e
sparktseung <hi@sparktseung.com>
2022-12-28 02:06:48 -0500

    test: loglik aggregation

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,41 +6,37 @@ using StatsFuns
 using Test
 
 const tests = [
-    # "dummytest",
-    # "pdfcdf",
-    # "expert_loglik",
-    # "loglik",
-    # "fit_functions"
-
+    # gating
     "gating",
-
+    # exposurize
     "experts/ll/exposurize",
-
+    # continuous experts
     "experts/continuous/burr",
     "experts/continuous/gamma",
     "experts/continuous/inversegaussian",
     "experts/continuous/lognormal",
     "experts/continuous/weibull",
-
+    # zi continuous experts
     "experts/continuous/ziburr",
     "experts/continuous/zigamma",
     "experts/continuous/ziinversegaussian",
     "experts/continuous/zilognormal",
     "experts/continuous/ziweibull",
-
+    # discrete experts
     "experts/discrete/binomial",
     "experts/discrete/gammacount",
     "experts/discrete/negativebinomial",
     "experts/discrete/poisson",
-
+    # zi discrete experts
     "experts/discrete/zibinomial",
     "experts/discrete/zigammacount",
     "experts/discrete/zinegativebinomial",
     "experts/discrete/zipoisson",
-
+    # loglik
+    "loglik",
 ]
 
-printstyled("Running tests:\n", color=:blue)
+printstyled("Running tests:\n"; color=:blue)
 
 for t in tests
     @testset "Test $t" begin
