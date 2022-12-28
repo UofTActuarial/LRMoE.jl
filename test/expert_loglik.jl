b45abe3f1b7947a2f58e0dc67e69fac444a4e330
sparktseung <hi@sparktseung.com>
2022-12-27 21:10:28 -0500

    remove old tests

--- test/expert_loglik.jl
+++ test/expert_loglik.jl
@@ -86,66 +86,6 @@ using LRMoE
 
 end
 
-@testset "expert_ll: continuous, NonZI" begin
-
-    # param1 = 3.0
-    # param2 = 2.0
-    # param3 = 5.0
-
-    # l = Distributions.LogNormal(param1, param2)
-    # x = rand(l, 20)
-
-    # tmp = LRMoE.BurrExpert(param1, param2, param3)
-
-    # @test isapprox(LRMoE.expert_ll_exact.(tmp, x), LRMoE.logpdf.(tmp, x))
-
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, x, x, Inf), LRMoE.logpdf.(tmp, x))
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0.0, 0.0, 0.0), LRMoE.logpdf.(tmp, 0.0))
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, x, Inf, Inf), logcdf.(tmp, Inf) .+ log1mexp.(logcdf.(tmp, x) .- logcdf.(tmp, Inf)), atol = 1e-6)
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0, Inf, Inf), logcdf.(tmp, Inf) .+ log1mexp.(logcdf.(tmp, 0) .- logcdf.(tmp, Inf)), atol = 1e-6)
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0.75 .*x, 1.25 .*x, Inf), logcdf.(tmp, 1.25 .*x) .+ log1mexp.(logcdf.(tmp, 0.75 .*x) .- logcdf.(tmp, 1.25 .*x)), atol = 1e-6)
-
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.0, x, x, Inf), fill(0.0, length(x)))
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.0, 0.0, 0.0, 0.0), -Inf)
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.25 .*x, 0.75 .*x, 1.25 .*x, 1.5 .*x), logcdf.(tmp, 1.5 .*x) .+ log1mexp.(logcdf.(tmp, 0.25 .*x) .- logcdf.(tmp, 1.5 .*x)), atol = 1e-6)
-
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.0, x, x, Inf), fill(-Inf, length(x)))
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.0, 0.0, 0.0, 0.0), 0.0)
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.25 .*x, 0.75 .*x, 1.25 .*x, 1.5 .*x), log1mexp.(logcdf.(tmp, 1.5 .*x) .+ log1mexp.(logcdf.(tmp, 0.25 .*x) .- logcdf.(tmp, 1.5 .*x))), atol = 1e-6)
-
-end
-
-@testset "expert_ll: continuous, ZI" begin
-
-    # param0 = rand(Distributions.Uniform(0, 1), 1)[1]
-    # param1 = 3.0
-    # param2 = 2.0
-    # param3 = 5.0
-
-    # l = Distributions.LogNormal(param1, param2)
-    # x = rand(l, 20) .+ 0.05
-
-    # tmp = LRMoE.ZIBurrExpert(param0, param1, param2, param3)
-
-    # @test isapprox(LRMoE.expert_ll_exact.(tmp, x), log(1-param0) .+ LRMoE.logpdf.(tmp, x))
-    # @test isapprox(LRMoE.expert_ll_exact.(tmp, 0), log(param0))
-
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, x, x, Inf), log.((1-param0).*exp.(LRMoE.logpdf.(tmp, x))))
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0.0, 0.0, 0.0), log.(param0))
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, x, Inf, Inf), log.((1-param0).*exp.(logcdf.(tmp, Inf) .+ log1mexp.(logcdf.(tmp, x) .- logcdf.(tmp, Inf)))), atol = 1e-6)
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0, Inf, Inf), log.(1), atol = 1e-6)
-    # @test isapprox(LRMoE.expert_ll.(tmp, 0.0, 0.75 .*x, 1.25 .*x, Inf), log.((1-param0).*exp.(logcdf.(tmp, 1.25 .*x) .+ log1mexp.(logcdf.(tmp, 0.75 .*x) .- logcdf.(tmp, 1.25 .*x)))), atol = 1e-6)
-
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.0, x, x, Inf), fill(0.0, length(x)))
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.0, 0.0, 0.0, 0.0), log(param0))
-    # @test isapprox(LRMoE.expert_tn.(tmp, 0.25 .*x, 0.75 .*x, 1.25 .*x, 1.5 .*x), log.((1-param0).*exp.(logcdf.(tmp, 1.5 .*x) .+ log1mexp.(logcdf.(tmp, 0.25 .*x) .- logcdf.(tmp, 1.5 .*x)))), atol = 1e-6)
-
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.0, x, x, Inf), fill(-Inf, length(x)))
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.0, 0.0, 0.0, 0.0), log(1-param0))
-    # @test isapprox(LRMoE.expert_tn_bar.(tmp, 0.25 .*x, 0.75 .*x, 1.25 .*x, 1.5 .*x), log.(param0 .+ (1-param0).*exp.(log1mexp.(logcdf.(tmp, 1.5 .*x) .+ log1mexp.(logcdf.(tmp, 0.25 .*x) .- logcdf.(tmp, 1.5 .*x))))), atol = 1e-6)
-
-end
-
 @testset "expert_ll: discrete, NonZI" begin
 
     # param1 = 20
@@ -435,31 +375,6 @@ end
     # param3 = rand(Distributions.Uniform(5, 10), 1)[1]
 
     # expo = rand(Distributions.Uniform(0.5, 20), 1)[1]
-
-    # tmp = LRMoE.BurrExpert(param1, param2, param3)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-    # tmp = LRMoE.ZIBurrExpert(param0, param1, param2, param3)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-
-    # tmp = LRMoE.GammaExpert(param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-    # tmp = LRMoE.ZIGammaExpert(param0, param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-
-    # tmp = LRMoE.InverseGaussianExpert(param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-    # tmp = LRMoE.ZIInverseGaussianExpert(param0, param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-
-    # tmp = LRMoE.LogNormalExpert(param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-    # tmp = LRMoE.ZILogNormalExpert(param0, param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-
-    # tmp = LRMoE.WeibullExpert(param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
-    # tmp = LRMoE.ZIWeibullExpert(param0, param1, param2)
-    # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
 
     # tmp = LRMoE.BinomialExpert(20, param0)
     # @test tmp == LRMoE.exposurize_expert(tmp, exposure = expo)
